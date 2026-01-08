96eb97d285a46ec0bfdc8bff616302d55a00e929
Sunny7042 <gsnehil40@gmail.com>
2026-01-08 19:16:12 +0530

    Update app.py

--- secret-print-demo/app.py
+++ secret-print-demo/app.py
@@ -3,23 +3,22 @@ import os
 
 app = Flask(__name__)
 
-SECRET_PATH = "/var/secrets/snehil-serv/password"
+path = "/var/secrets/snehil-serv/password"
 
-secret_value = "SECRET_NOT_FOUND"
-
-if os.path.exists(SECRET_PATH):
-    with open(SECRET_PATH, "r") as f:
-        secret_value = f.read().strip()
-
-# PRINT SECRET IN LOGS
-print(f"[SECRET] {secret_value}")
+if os.path.exists(path):
+    with open(path, "r") as f:
+        value = f.read().strip()
+    print(f"[SECRET] {value}")
+else:
+    print("[SECRET] File not found:", path)
 
 @app.route("/")
 def index():
-    return "Service started. Check logs for secret value."
+    return "Check logs for secret value."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
 
 
 
